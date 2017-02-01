45ea663582919d2239792873b49c2b71a30f1c96
Ru Tan <jutan2012@gmail.com>
2017-02-01 14:19:20 +0100

    Faire déteindre les chaussettes rouges en rose

--- index_idiorm.php
+++ index_idiorm.php
@@ -5,11 +5,10 @@ ORM::configure('username', 'root');
 ORM::configure('password', 'root');
 $chaussettes = ORM::for_table('mes_chaussettes')->where('couleur', 'rouge')->find_many();
 //Mettre la condition pour afficher la couleur rouge 
-foreach($chaussettes as $mesChaussettes){
-	echo $mesChaussettes->id.'<br>';
-	echo $
+foreach($chaussettes as $chaussette){
+
+	echo $chaussette->id.'<br>';
+	echo $chaussette->couleu = 'rose'.'<br>';
+	echo $chaussette->save();
 }
-//var_dump($chaussettes);
 
-
- ?>
